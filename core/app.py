40e56ee81fc24b52032e1d46d09da76b6bf5f93e
MGRINZ <24416088+MGRINZ@users.noreply.github.com>
2022-05-10 22:38:35 +0200

    Fetching all data from website
    - Request website and extract timetable rows
    - Create class Lesson and parse data
    - Remove placeholder output
    - Create schedule dict with empty array and append all Lessons to it
    - Rename some variables from Polish to English
    - Handle request errors

--- core/app.py
+++ core/app.py
@@ -1,37 +1,88 @@
 from flask import Flask
 from flask import request
 import os
-import json
+import requests
+from bs4 import BeautifulSoup, Tag
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # http://127.0.0.1:5000/?dzien=&godziny=&instruktor=&kurs=&poziom=&uwagi=&zapisy=
+    # http://127.0.0.1:5000/?dzien=&godziny=&kurs=&poziom=&uwagi=&instruktor=&zapisy=
     
-    dzien = request.args.get("dzien")
-    godziny = request.args.get("godziny")
-    instruktor = request.args.get("instruktor")
-    kurs = request.args.get("kurs")
-    poziom = request.args.get("poziom")
-    uwagi = request.args.get("uwagi")
-    zapisy = request.args.get("zapisy")
+    day = request.args.get("dzien")
+    time = request.args.get("godziny")
+    course = request.args.get("kurs")
+    level = request.args.get("poziom")
+    notes = request.args.get("uwagi")
+    instructor = request.args.get("instruktor")
+    enrollment = request.args.get("zapisy")
 
     if os.getenv("FLASK_ENV") == "development":
-        print(f"dzien={dzien}")
-        print(f"godziny={godziny}")
-        print(f"instruktor={instruktor}")
-        print(f"kurs={kurs}")
-        print(f"poziom={poziom}")
-        print(f"uwagi={uwagi}")
-        print(f"zapisy={zapisy}")
+        print(f"dzien={day}")
+        print(f"godziny={time}")
+        print(f"kurs={course}")
+        print(f"poziom={level}")
+        print(f"uwagi={notes}")
+        print(f"instruktor={instructor}")
+        print(f"zapisy={enrollment}")
     
-    return get_schedule(dzien, godziny, instruktor, kurs, poziom, uwagi, zapisy)
+    return get_schedule(day, time, course, level, notes, instructor, enrollment)
 
-def get_schedule(dzien: str, godziny: str, instruktor: str, kurs: str, poziom: str, uwagi: str, zapisy: str) -> dict:
+def get_schedule(day: str, time: str, course: str, level: str, notes: str, instructor: str, enrollment: str) -> dict:
+
+    response = requests.get("https://www.dancefusion.com.pl/grafik/")
+
+    if response.status_code != 200:
+        return get_error(response.status_code)
+
+    bs = BeautifulSoup(response.content, 'html.parser')
+    table: Tag = bs.find("table", id="table_1")
+    trs: list[Tag] = table.tbody.find_all("tr")
     
-    # Return example response
-    with open("../response.example.json", "r") as f:
-        schedule = json.load(f)
+    schedule = {
+        "status": "sukces",
+        "grafik": []
+    }
+
+    for tr in trs:
+        tds: list[Tag] = tr.find_all("td")
+        lesson = Lesson(tds)
+        schedule["grafik"].append(lesson.as_dict())
     
     return schedule
+
+def get_error(status_code: int) -> dict:
+    error = {
+        "status": "błąd",
+    }
+
+    if status_code is not None:
+        error["kod"] = status_code
+
+    return error
+
+class Lesson:
+
+    def __init__(self, tds: list[Tag]):
+        self.day = tds[1].text
+        self.time = tds[2].text
+        self.course = tds[3].text
+        self.level = tds[4].text
+        self.notes = [ s for s in tds[5].strings ]
+        self.instructor = tds[7].text
+        self.enrollment = " ".join([ s for s in tds[8].strings ])
+
+        if self.enrollment == "ZAPISZ SIĘ":
+            self.enrollment = "WOLNE MIEJSCA"
+
+    def as_dict(self) -> dict:
+        return {
+            "dzien": self.day,
+            "godziny": self.time,
+            "kurs": self.course,
+            "poziom": self.level,
+            "uwagi": self.notes,
+            "instruktor": self.instructor,
+            "zapisy": self.enrollment
+        }
